be921d1ccf237240017967f707290f3b55db5815
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-10-14 18:29:27 +0200

    Implementazione funzionalità CaricaFile

--- AS2223_4G_INF_Cangiotti_RubricaCSVMAUI/MainPage.xaml.cs
+++ AS2223_4G_INF_Cangiotti_RubricaCSVMAUI/MainPage.xaml.cs
@@ -7,9 +7,45 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void btnCaricaFile_Clicked(object sender, EventArgs e)
-	{
+	static int nRecord = 0;
+	Contatto[] contatti = new Contatto[nRecord];
 
+	void CaricaFileCSV()
+	{
+		using (StreamReader sr = new StreamReader(txtFile.Text))
+		{
+			string riga;
+			int i = 0;
+			while (!sr.EndOfStream)
+			{
+				riga = sr.ReadLine();
+				Contatto contatto = new Contatto(riga);
+				contatti[i] = contatto;
+
+				i++;
+			}
+		}
+    }
+
+	private async void btnCaricaFile_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			var file = await FilePicker.PickAsync();
+			if (file == null)
+			{
+				return;
+			}
+			txtFile.Text = file.FullPath;
+            nRecord = File.ReadLines(txtFile.Text).Count();
+            Array.Resize(ref contatti, nRecord);
+
+            CaricaFileCSV();
+		}
+		catch
+		{
+            await DisplayAlert("Errore", "Impossibile caricare il file", "OK");
+        }
 	}
 
 	private void btnVisualizza_Clicked(object sender, EventArgs e)

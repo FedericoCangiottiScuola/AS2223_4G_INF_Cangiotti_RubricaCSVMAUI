285450e5e44731810b4ac624d2aa3b3625d5f0f9
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-10-14 18:09:53 +0200

    Aggiunta classe Contatto

---
+++ AS2223_4G_INF_Cangiotti_RubricaCSVMAUI/Contatto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_Cangiotti_RubricaCSVMAUI
+{
+    internal class Contatto
+    {
+        string nome;
+        string cognome;
+        string citta;
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getCognome()
+        {
+            return cognome;
+        }
+
+        public string getCitta()
+        {
+            return citta;
+        }
+
+        public Contatto(string record)
+        {
+            string[] supporto = record.Split(",");
+            cognome = supporto[0];
+            nome = supporto[1];
+            citta = supporto[2];
+        }
+
+        public Contatto(string nome, string cognome, string citta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.citta = citta;
+        }
+    }
+}

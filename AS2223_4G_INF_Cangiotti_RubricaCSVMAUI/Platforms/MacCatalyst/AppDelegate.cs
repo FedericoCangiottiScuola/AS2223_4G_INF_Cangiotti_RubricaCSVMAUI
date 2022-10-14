2aad08f0ea3a140141e6d9967f411c5b17a31b67
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-10-14 15:04:03 +0200

    Add project files.

---
+++ AS2223_4G_INF_Cangiotti_RubricaCSVMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AS2223_4G_INF_Cangiotti_RubricaCSVMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
